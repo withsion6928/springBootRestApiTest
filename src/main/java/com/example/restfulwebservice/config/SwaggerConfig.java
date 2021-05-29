e4cf4a6485cfc735fe56535e144d3e1dac6baddb
mtJu <with6928@gmail.com>
2021-05-30 01:28:53 +0900

    20210529  SwaggerConfig.java 추가           Swagger2 / swagger-ui 추가           documentation 구현

--- src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
+++ src/main/java/com/example/restfulwebservice/config/SwaggerConfig.java
@@ -6,21 +6,70 @@ import org.springframework.hateoas.client.LinkDiscoverer;
 import org.springframework.hateoas.client.LinkDiscoverers;
 import org.springframework.hateoas.mediatype.collectionjson.CollectionJsonLinkDiscoverer;
 import org.springframework.plugin.core.SimplePluginRegistry;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
-    @Bean
-    public LinkDiscoverers  api(){
-//        return new Docket(DocumentationType.SWAGGER_2);
 
+    public static Contact DEFAULT_CONTACT = new  Contact("mtJu","https://withsion.or.kr","with6928@gmail.com");
+
+
+    private static final ApiInfo  DEFAULT_API_INFO = new ApiInfo("Awesom API Tittle","My user management RestApi service"
+    , "1.0","urn:tos",DEFAULT_CONTACT,"Apache 2.0", "https://www.apache.org/licenses/LICENSE-2.0"
+    ,new ArrayList<>());
+
+    private static final Set<String> DEFAULT_PRODUCES_AND_CONSUMES = new HashSet<>(
+            Arrays.asList("application/json", "application/xml"));
+
+
+//
+//    @Bean
+//    public Docket api(){
+//        return new Docket(DocumentationType.SWAGGER_2)
+//                .apiInfo(DEFAULT_API_INFO)
+//                .produces(DEFAULT_PRODUCES_AND_CONSUMES)
+//                .consumes(DEFAULT_PRODUCES_AND_CONSUMES);
+//
+//    }
+
+    @Bean
+    public LinkDiscoverers LinkDiscoverers(){
         List<LinkDiscoverer> plugins = new ArrayList<>();
         plugins.add(new CollectionJsonLinkDiscoverer());
         return new LinkDiscoverers(SimplePluginRegistry.create(plugins));
+    }
+
+    @Bean
+    public Docket postsApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("SwaggerConfig")
+                .apiInfo(buildApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.regex("/.*"))
+                .build();
+    }
+
+    private ApiInfo buildApiInfo() {
+        Contact contact = new Contact("mtJu", "https://withsion.or.kr", "with6928@gmail.com");
+        return new ApiInfoBuilder()
+                .title("SwaggerConfig")
+                .description("API Description")
+                .license("license")
+                .version("1.0")
+                .contact(contact)
+                .licenseUrl("https://www.apache.org/licenses/LICENSE-2.0")
+                .build();
     }
 }
 
--- src/main/java/com/example/restfulwebservice/user/User.java
+++ src/main/java/com/example/restfulwebservice/user/User.java
@@ -3,6 +3,8 @@ package com.example.restfulwebservice.user;
 import com.fasterxml.jackson.annotation.JsonFilter;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -12,15 +14,18 @@ import java.util.Date;
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@ApiModel(description = "사용자 상세정보를 위한 도메인 객체")
 //@JsonIgnoreProperties(value = {"password"})
 //@JsonFilter("UserInfo")
 public class User {
     private Integer id;
 
+    @ApiModelProperty(notes = "사용자 이름을 입력해 주세요")
     private String name;
-
+    @ApiModelProperty(notes = "등록일을 입력해 주세요")
     private Date joinDate;
-
+    @ApiModelProperty(notes = "사용자 패스워드를 입력해 주세요")
     private String password;
+    @ApiModelProperty(notes = "주민번호를 입력해 주세요")
     private String ssn;
  }
